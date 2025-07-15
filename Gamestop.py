430eae5a0cf547e3559cf908e22114d8a48140cd
Ehsan5o <ehsan.omidvari@gmail.com>
2025-07-16 00:58:45 +0400

    Add files via upload

---
+++ Gamestop.py
@@ -0,0 +1,60 @@
+import yfinance as yf
+import pandas as pd
+import requests
+from bs4 import BeautifulSoup
+import plotly.graph_objects as go
+from plotly.subplots import make_subplots
+import plotly.io as pio
+pio.renderers.default = "iframe"
+import warnings
+# Ignore all warnings
+warnings.filterwarnings("ignore", category=FutureWarning)
+def make_graph(stock_data, revenue_data, stock):
+    fig = make_subplots(rows=2, cols=1, shared_xaxes=True, subplot_titles=("Historical Share Price", "Historical Revenue"), vertical_spacing = .3)
+    stock_data_specific = stock_data[stock_data.Date <= '2021-06-14']
+    revenue_data_specific = revenue_data[revenue_data.Date <= '2021-04-30']
+    fig.add_trace(go.Scatter(x=pd.to_datetime(stock_data_specific.Date), y=stock_data_specific.Close.astype("float"), name="Share Price"), row=1, col=1)
+    fig.add_trace(go.Scatter(
+    x=pd.to_datetime(revenue_data_specific.Date),  # Remove infer_datetime_format argument
+    y=revenue_data_specific.Revenue.astype("float"),
+    name="Revenue"
+), row=2, col=1)
+    fig.update_xaxes(title_text="Date", row=1, col=1)
+    fig.update_xaxes(title_text="Date", row=2, col=1)
+    fig.update_yaxes(title_text="Price ($US)", row=1, col=1)
+    fig.update_yaxes(title_text="Revenue ($US Millions)", row=2, col=1)
+    fig.update_layout(showlegend=False,
+    height=900,
+    title=stock,
+    xaxis_rangeslider_visible=True)
+    fig.show()
+    from IPython.display import display, HTML
+    fig_html = fig.to_html()
+    display(HTML(fig_html))
+url2 = "https://cf-courses-data.s3.us.cloud-object-storage.appdomain.cloud/IBMDeveloperSkillsNetwork-PY0220EN-SkillsNetwork/labs/project/stock.html"
+html_data_2 = requests.get(url2).text
+beautiful_soup = BeautifulSoup(html_data_2, "html.parser")
+
+# Create the DataFrame
+gme_revenue = pd.DataFrame(columns=["Date", "Revenue"])
+
+# Extract data from the table
+for row in beautiful_soup.find_all("tbody")[1].find_all("tr"):
+    col = row.find_all("td")
+    date = col[0].text
+    revenue = col[1].text
+    gme_revenue = pd.concat([gme_revenue, pd.DataFrame({"Date": [date], "Revenue": [revenue]})], ignore_index=True)
+gme_revenue["Revenue"] = gme_revenue['Revenue'].str.replace(',|\$', "", regex=True)
+gme_revenue.dropna(inplace=True)
+gme_revenue = gme_revenue[gme_revenue['Revenue'] != ""]
+
+print("Last five rows of the GameStop stock revenue data:")
+print(gme_revenue.tail()[["Date", "Revenue"]])
+
+# Download GameStop stock data using yfinance
+gme = yf.Ticker("GME")
+gme_data = gme.history(period="max")
+gme_data.reset_index(inplace=True)
+gme_data['Date'] = gme_data['Date'].dt.strftime('%Y-%m-%d')
+
+make_graph(gme_data, gme_revenue, 'GameStop')
